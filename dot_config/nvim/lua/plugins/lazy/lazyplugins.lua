93cde975b3ca8947f6be3f90c8f60d6004d8eebe
Jack Hamilton <jackham800@gmail.com>
2024-07-09 17:21:35 -0500

    tweaks

--- dot_config/nvim/lua/plugins/lazy/lazyplugins.lua
+++ dot_config/nvim/lua/plugins/lazy/lazyplugins.lua
@@ -267,7 +267,29 @@ return {
         config = function()
             require("xcodebuild").setup({
                 -- put some options here or leave it empty to use default settings
+
             })
+            vim.keymap.set("n", "<leader>Xf", "<cmd>XcodebuildProjectManager<cr>",
+                { desc = "Show Project Manager Actions" })
+
+            vim.keymap.set("n", "<leader>Xb", "<cmd>XcodebuildBuild<cr>", { desc = "Build Project" })
+            vim.keymap.set("n", "<leader>XB", "<cmd>XcodebuildBuildForTesting<cr>", { desc = "Build For Testing" })
+            vim.keymap.set("n", "<leader>Xr", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run Project" })
+            vim.keymap.set("n", "<leader>Xt", "<cmd>XcodebuildTest<cr>", { desc = "Run Tests" })
+            vim.keymap.set("v", "<leader>Xt", "<cmd>XcodebuildTestSelected<cr>", { desc = "Run Selected Tests" })
+            vim.keymap.set("n", "<leader>XT", "<cmd>XcodebuildTestClass<cr>", { desc = "Run Current Test Class" })
+            vim.keymap.set("n", "<leader>X.", "<cmd>XcodebuildTestRepeat<cr>", { desc = "Repeat Last Test Run" })
+            vim.keymap.set("n", "<leader>Xl", "<cmd>XcodebuildToggleLogs<cr>", { desc = "Toggle Xcodebuild Logs" })
+            vim.keymap.set("n", "<leader>Xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", { desc = "Toggle Code Coverage" })
+            vim.keymap.set("n", "<leader>XC", "<cmd>XcodebuildShowCodeCoverageReport<cr>",
+                { desc = "Show Code Coverage Report" })
+            vim.keymap.set("n", "<leader>Xe", "<cmd>XcodebuildTestExplorerToggle<cr>", { desc = "Toggle Test Explorer" })
+            vim.keymap.set("n", "<leader>Xs", "<cmd>XcodebuildFailingSnapshots<cr>", { desc = "Show Failing Snapshots" })
+            vim.keymap.set("n", "<leader>Xd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
+            vim.keymap.set("n", "<leader>Xp", "<cmd>XcodebuildSelectTestPlan<cr>", { desc = "Select Test Plan" })
+            vim.keymap.set("n", "<leader>Xq", "<cmd>Telescope quickfix<cr>", { desc = "Show QuickFix List" })
+            vim.keymap.set("n", "<leader>Xx", "<cmd>XcodebuildQuickfixLine<cr>", { desc = "Quickfix Line" })
+            vim.keymap.set("n", "<leader>Xa", "<cmd>XcodebuildCodeActions<cr>", { desc = "Show Code Actions" })
         end,
     },
     -- Debugging
@@ -284,20 +306,6 @@ return {
             local codelldbPath = os.getenv("HOME") .. "/Documents/codelldb-aarch64-darwin.vsix"
 
             xcodebuild.setup(codelldbPath)
-
-            local wk = require('which-key')
-            wk.register({
-                d = {
-                    name = "Debugger",
-                    d = { xcodebuild.build_and_debug, "Build & Debug" },
-                    r = { xcodebuild.debug_without_build, "Debug without building" },
-                    T = { xcodebuild.debug_class_tests, "Debug Class Tests" },
-                    b = { xcodebuild.toggle_breakpoint, "Toggle Breakpoint" },
-                    B = { xcodebuild.toggle_message_breakpoint, "Toggle message breakpoint" },
-                    x = { xcodebuild.terminate_session, "Terminate Debugger" },
-                }
-
-            }, { prefix = "<leader>" })
         end,
     },
     {
