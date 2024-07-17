1ccceef480405453e2ca07e025570b5755580de2
Jack Hamilton <jackham800@gmail.com>
2024-07-17 11:37:14 -0500

    theme switcher, colemak remaps

--- dot_config/nvim/lua/plugins/lazy/theme.lua
+++ dot_config/nvim/lua/plugins/lazy/theme.lua
@@ -1,15 +1,62 @@
 return {
-    { 'christianchiarulli/nvcode-color-schemes.vim' },
-    { 'Th3Whit3Wolf/onebuddy' },
-    { 'sainnhe/edge' },
+    {
+        'christianchiarulli/nvcode-color-schemes.vim',
+        lazy = true,
+    },
+    {
+        'sainnhe/edge',
+        lazy = true,
+    },
     {
         'sainnhe/sonokai',
+        lazy = true,
     },
-    { 'EdenEast/nightfox.nvim' },
+    {
+        'EdenEast/nightfox.nvim',
+        lazy = true,
+    },
     {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
         opts = {},
     },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000
+    },
+    {
+        'Everblush/nvim',
+        name = 'everblush',
+        lazy = true
+    },
+    {
+        "rose-pine/neovim",
+        as = "rose-pine",
+        lazy = true
+    },
+    {
+        "sainnhe/gruvbox-material",
+        as = "gruvbox-material",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = true,
+    },
+    {
+        "sainnhe/everforest",
+        lazy = true,
+    },
+    {
+        "vague2k/vague.nvim",
+        lazy = true,
+    },
+    {
+        "morhetz/gruvbox",
+        lazy = true,
+    }
 }
