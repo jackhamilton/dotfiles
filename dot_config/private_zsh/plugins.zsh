143bdd824b2d3c73215c518350eabf6fdf08593e
Jack Hamilton <jackham800@gmail.com>
2024-07-26 14:05:37 -0500

    refactoring

--- dot_config/private_zsh/plugins.zsh
+++ dot_config/private_zsh/plugins.zsh
@@ -1,18 +1,17 @@
-zstyle ':znap:*' repos-dir ~/.znap/Repos/
+znap source marlonrichert/zsh-autocomplete
 
-lsdloc=$(which lsd)
-AUTO_LS_COMMANDS=(git-status $lsdloc)
-ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd completion)
+#ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd completion)
+ZSH_AUTOSUGGEST_STRATEGY=(history)
+znap source zsh-users/zsh-autosuggestions
 
 znap source zsh-users/zsh-syntax-highlighting
+
 znap source akash329d/zsh-alias-finder
-#znap source Junker/zsh-archlinux@main
+lsdloc=$(which lsd)
+AUTO_LS_COMMANDS=(git-status $lsdloc)
 znap source desyncr/auto-ls
 znap source zpm-zsh/undollar
 znap source jeffreytse/zsh-vi-mode
-znap source zsh-users/zsh-autosuggestions
-znap source marlonrichert/zsh-autocomplete
-
-znap install aureliojargas/clitest zsh-users/zsh-completions
-
-source ~/.znap/Repos/marlonrichert/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+#znap source Junker/zsh-archlinux@main
+#
+znap install zsh-users/zsh-completions
