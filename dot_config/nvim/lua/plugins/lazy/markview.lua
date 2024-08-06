6460db249c1a8a3d22cc7915cbabff9adec9aefb
Jack Hamilton <jackham800@gmail.com>
2024-08-05 22:19:22 -0500

    grug-far

--- dot_config/nvim/lua/plugins/lazy/markview.lua
+++ dot_config/nvim/lua/plugins/lazy/markview.lua
@@ -1,11 +1,10 @@
 return {
---     {
---         "OXY2DEV/markview.nvim",
---         dependencies = {
---             "nvim-tree/nvim-web-devicons",
---         },
---         config = function()
---             require("markview").setup()
---         end,
---     },
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 }
