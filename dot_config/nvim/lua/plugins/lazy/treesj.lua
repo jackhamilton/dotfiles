a69d4d2511e9e601503d73ff51c617eb43c8d440
Jack Hamilton <jackham800@gmail.com>
2025-08-19 14:10:45 -0500

    plugin updates

---
+++ dot_config/nvim/lua/plugins/lazy/treesj.lua
@@ -0,0 +1,14 @@
+return {
+    -- {
+    --     'Wansmer/treesj',
+    --     keys = {
+    --         'gs',
+    --         'gj',
+    --         'gm'
+    --     },
+    --     dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+    --     config = function()
+    --         require('treesj').setup({ --[[ your config ]] })
+    --     end,
+    -- },
+}
