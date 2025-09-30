5e138fe18778dd7e2f6b4ea2d44d6675955b10e7
Jack Hamilton <jackham800@gmail.com>
2025-09-30 13:31:58 -0500

    Many updates to dotfiles

--- dot_config/nvim/lua/core/commands.lua
+++ dot_config/nvim/lua/core/commands.lua
@@ -2,17 +2,6 @@ vim.api.nvim_create_user_command("SudoWrite", function()
   require("utils.sudo_write").write()
 end, { desc = "Write file with sudo permissions" })
 
-vim.api.nvim_create_user_command("XCRebuild", function()
-  require("utils.sudo_write").write()
-    Snacks.notify.info("Started XCode rebuild")
-    local Job = require("plenary.job")
-    local job = Job:new({
-	    cwd = "/Users/jackhamilton/Documents/GitHub/grindr/",
-	    command = "xcreset",
-	    on_exit = function(j, return_val)
-            Snacks.notify.info("XCode rebuild finished")
-	    end,
-    })
-    job:start()
-end, { desc = "Rebuild XCWorkspace and regenerate buildServer.json" })
-
+vim.api.nvim_create_user_command("DeleteMarks", function()
+    vim.cmd [[delmarks a-zA-Z0-9]]
+end, { desc = "Write file with sudo permissions" })
