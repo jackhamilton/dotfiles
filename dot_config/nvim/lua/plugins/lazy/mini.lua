e22de831f9816aa570d68634c34ca91b67b07eab
jackhamilton <jackham800@gmail.com>
2025-12-22 18:08:15 -0500

    Stable changes p2

--- dot_config/nvim/lua/plugins/lazy/mini.lua
+++ dot_config/nvim/lua/plugins/lazy/mini.lua
@@ -1,9 +1,6 @@
 return {
     {
         'echasnovski/mini.nvim',
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects"
-        },
         version = false,
     },
 }
