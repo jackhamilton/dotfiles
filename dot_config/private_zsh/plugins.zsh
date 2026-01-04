c96a791bcb612e5995c29f9d576ebc539793ee8e
Jack Hamilton <jackham800@gmail.com>
2026-01-03 22:04:05 -0600

    add some stuff

--- dot_config/private_zsh/plugins.zsh
+++ dot_config/private_zsh/plugins.zsh
@@ -1,15 +1,13 @@
 znap source marlonrichert/zsh-autocomplete
 
 #ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd completion)
-ZSH_AUTOSUGGEST_STRATEGY=(history)
+ZSH_AUTOSUGGEST_STRATEGY=(completion history)
 znap source zsh-users/zsh-autosuggestions
 
 znap source zsh-users/zsh-syntax-highlighting
 
 znap source akash329d/zsh-alias-finder
 lsdloc=$(which lsd)
-# AUTO_LS_COMMANDS=(git-status $lsdloc)
-znap source desyncr/auto-ls
 znap source zpm-zsh/undollar
 znap source jeffreytse/zsh-vi-mode
 znap source nix-community/nix-zsh-completions
