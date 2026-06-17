4f73c66ea3e432c4ce531d84c9049e056baaa067
Jack Hamilton <jhamilton@superfile.com>
2026-06-17 10:28:30 -0500

    Vim and tmux updates

--- dot_config/nvim/lua/plugins/lazy/hardtime.lua
+++ dot_config/nvim/lua/plugins/lazy/hardtime.lua
@@ -1,25 +1,25 @@
 return {
-	{
-		"m4xshen/hardtime.nvim",
-		lazy = false,
-		dependencies = { "MunifTanjim/nui.nvim" },
-		opts = {
-		    restricted_keys = {
-                ["<Up>"] = { "n", "x" },
-                ["<Down>"] = { "n", "x" },
-                ["<Left>"] = { "n", "x" },
-                ["<Right>"] = { "n", "x" },
-                ["h"] = false,
-                ["j"] = false,
-                ["k"] = false,
-                ["l"] = false,
-		    },
-		    disabled_keys = {
-                ["<Up>"] = false, -- Allow <Up> key
-                ["<Down>"] = false,
-                ["<Left>"] = false,
-                ["<Right>"] = false,
-		    },
-		}
-	},
+	-- {
+	-- 	"m4xshen/hardtime.nvim",
+	-- 	lazy = false,
+	-- 	dependencies = { "MunifTanjim/nui.nvim" },
+	-- 	opts = {
+	-- 	    restricted_keys = {
+ --                ["<Up>"] = { "n", "x" },
+ --                ["<Down>"] = { "n", "x" },
+ --                ["<Left>"] = { "n", "x" },
+ --                ["<Right>"] = { "n", "x" },
+ --                ["h"] = false,
+ --                ["j"] = false,
+ --                ["k"] = false,
+ --                ["l"] = false,
+	-- 	    },
+	-- 	    disabled_keys = {
+ --                ["<Up>"] = false, -- Allow <Up> key
+ --                ["<Down>"] = false,
+ --                ["<Left>"] = false,
+ --                ["<Right>"] = false,
+	-- 	    },
+	-- 	}
+	-- },
 }
